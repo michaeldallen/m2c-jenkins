d5213f4e5e45409bf2bc8b5eb0e68f767886ac9d
Michael Allen <michael@allenhub.net>
2019-08-05 11:07:12 +0100

    Henry note

---
+++ henry.py
@@ -0,0 +1,14 @@
+def main():
+    you = "Henry"
+    me = "Dady"
+    what = "Python camp"
+    
+    print(f"Hello, {you}!")
+    print(f"Have fun at {what} :)")
+    print(f"I love you!\n{me}")
+    
+
+if __name__ == '__main__':
+    main()
+
+    
